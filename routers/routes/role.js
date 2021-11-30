b1b218d747dbcdfb6aef13d9afb48959f7f43dfc
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-30 13:09:19 +0300

    routes role

---
+++ routers/routes/role.js
@@ -0,0 +1,11 @@
+// عمل اكسبورت للاكسبريس بداخل الروتز رول
+const express = require("express");
+// تعريف ثابت رول راوتر لاستخدامها في القيت والبوست
+const roleRouter = express.Router();
+// استدعاء مابداخل الكنترول  من انشاء وعرض
+const { newrolr, getrole } = require("../controllers/role");
+// الباثات التي تستخدمها في البسوت مان للعرض والانشاء
+roleRouter.post("/role", newrolr);
+roleRouter.get("/read", getrole);
+// عمل اكسبورت roleRouter
+module.exports = roleRouter;
