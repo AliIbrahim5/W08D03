e7a6c98b8f78b839f29263ef9cdf056207fef648
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-30 13:05:58 +0300

    user

---
+++ db/models/user.js
@@ -0,0 +1,11 @@
+// ريكواير للمونقوز
+const mongoose = require("mongoose");
+// انشاء سكيما فيها الايميل والباسورد وايضا الرول
+const user = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, require: true },
+  role: [{ type: mongoose.Schema.Types.ObjectId, ref: "Role" }],
+});
+
+// اكسبورت لليوزر
+module.exports = mongoose.model("User", user);
